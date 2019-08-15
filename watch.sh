33c430b93ab8987f5c09d4bbc580cf4b47ee9f1f
Paul Brodner <paul.brodner@gmail.com>
2019-08-15 19:12:20 +0300

    update skaffold and watch script

--- watch.sh
+++ watch.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
 # watch the java files and continously deploy the service
-mvn clean install
-skaffold run -p dev
-reflex -r "\.java$" -- bash -c 'mvn install && skaffold run -p dev'
+mvn clean install && skaffold run -p dev
